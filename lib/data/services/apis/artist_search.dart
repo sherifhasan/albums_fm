bc7f960b08a518833ec8559db5f2fde9ce3d557b
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-08 14:58:59 +0200

    add artist search api

---
+++ lib/data/services/apis/artist_search.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:appsfactory_task/data/models/artist_search_response.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'api_config.dart';
+
+class MerchantApi {
+  Future<ArtistSearchResponse?> search(String artist) async {
+    final client = Dio();
+    final url = '${ApiMethods.artistSearch}$artist';
+    try {
+      final response = await client.get(url,
+          options: Options(responseType: ResponseType.json));
+      if (response.statusCode == 200 && response.data != null) {
+        return compute(parseResponse, response.data);
+      } else {
+        debugPrint('${response.statusCode} : ${response.data.toString()}');
+      }
+    } on DioError catch (error) {
+      debugPrint(error.message);
+    } finally {
+      client.clear();
+    }
+    return null;
+  }
+}
+
+ArtistSearchResponse parseResponse(response) =>
+    ArtistSearchResponse.fromJson(response);
