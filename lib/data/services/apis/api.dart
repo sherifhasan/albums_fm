8d1afced9428a6340e3dc92683335063a51e6976
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-08 15:35:53 +0200

    implement artist top albums api

--- lib/data/services/apis/api.dart
+++ lib/data/services/apis/api.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 
-import '../../models/artist_search_response.dart';
+import 'package:appsfactory_task/data/models/artist_top_albums_response.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../../models/artist_search_response.dart';
 import 'api_config.dart';
 
 class Api {
@@ -15,7 +16,26 @@ class Api {
       final response = await client.get(url,
           options: Options(responseType: ResponseType.json));
       if (response.statusCode == 200 && response.data != null) {
-        return compute(parseResponse, response.data);
+        return compute(parseSearchResponse, response.data);
+      } else {
+        debugPrint('${response.statusCode} : ${response.data.toString()}');
+      }
+    } on DioError catch (error) {
+      debugPrint(error.message);
+    } finally {
+      client.clear();
+    }
+    return null;
+  }
+
+  Future<ArtistTopAlbums?> getTopAlbums(String artist) async {
+    final client = Dio();
+    final url = '${ApiMethods.topAlbums}$artist';
+    try {
+      final response = await client.get(url,
+          options: Options(responseType: ResponseType.json));
+      if (response.statusCode == 200 && response.data != null) {
+        return compute(parseArtistTopAlbumsResponse, response.data);
       } else {
         debugPrint('${response.statusCode} : ${response.data.toString()}');
       }
@@ -28,5 +48,8 @@ class Api {
   }
 }
 
-ArtistSearchResponse parseResponse(response) =>
+ArtistSearchResponse parseSearchResponse(response) =>
     ArtistSearchResponse.fromJson(response);
+
+ArtistTopAlbums parseArtistTopAlbumsResponse(response) =>
+    ArtistTopAlbums.fromJson(response);
