77661a9c0c3852b6a4ee6a3419d312f888753b4b
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-10 21:12:05 +0200

    refactor search screen view

--- lib/core/screens/top_albums/top_album_screen.dart
+++ lib/core/screens/top_albums/top_album_screen.dart
@@ -1,16 +1,14 @@
-import 'package:appsfactory_task/core/actions/top_albums/top_albums_fetch_action.dart';
-import 'package:appsfactory_task/core/models/app_state.dart';
-import 'package:appsfactory_task/core/screens/top_albums/widgets/top_albums_populated_view.dart';
-import 'package:appsfactory_task/core/screens/widgets/empty_view.dart';
-import 'package:appsfactory_task/core/screens/widgets/loading_view.dart';
-import 'package:appsfactory_task/data/models/artist_top_albums_response.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
-import '../../../router.dart';
-import '../../../shared/routes.dart';
+import '../../../data/models/artist_top_albums_response.dart';
+import '../../actions/top_albums/top_albums_fetch_action.dart';
+import '../../models/app_state.dart';
+import '../widgets/empty_view.dart';
+import '../widgets/loading_view.dart';
+import 'widgets/top_albums_populated_view.dart';
 
 class TopAlbumsScreen extends StatelessWidget {
   final String artist;
@@ -27,15 +25,6 @@ class TopAlbumsScreen extends StatelessWidget {
             appBar: AppBar(
               title: Text(artist),
               centerTitle: true,
-              actions: [
-                IconButton(
-                    onPressed: () =>
-                        navigationService.pushNamed(RoutePaths.search),
-                    icon: const Icon(
-                      CupertinoIcons.search,
-                      color: Colors.white,
-                    ))
-              ],
             ),
             body: _buildVisible(store.state),
           );
