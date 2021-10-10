c630e7c001d5d2a656ae7ae91786c87e38d5260f
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-10 18:53:56 +0200

    add top albums test dummy data

---
+++ test/data/models/top_albums/dummy_data.dart
@@ -0,0 +1,42 @@
+const artistTopAlbumsJsonString = """{
+  "topalbums": {
+    "album": [
+      {
+        "name": "Dreaming of You",
+        "playcount": 1049664,
+        "mbid": "8a520bd8-3d7c-3901-9850-bc8804db198b",
+        "url": "https://www.last.fm/music/Selena/Dreaming+of+You",
+        "artist": {
+          "name": "Selena",
+          "mbid": "04b97d63-b900-4e2e-92dc-a4331ddc95a6",
+          "url": "https://www.last.fm/music/Selena"
+        },
+        "image": [
+          {
+            "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2b03aa5e2baae31cfa94422fca09f5a2.png",
+            "size": "small"
+          },
+          {
+            "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2b03aa5e2baae31cfa94422fca09f5a2.png",
+            "size": "medium"
+          },
+          {
+            "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2b03aa5e2baae31cfa94422fca09f5a2.png",
+            "size": "large"
+          },
+          {
+            "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2b03aa5e2baae31cfa94422fca09f5a2.png",
+            "size": "extralarge"
+          }
+        ]
+      }
+    ],
+    "@attr": {
+      "artist": "Selena",
+      "page": "1",
+      "perPage": "1",
+      "totalPages": "4573",
+      "total": "4573"
+    }
+  }
+} """;
