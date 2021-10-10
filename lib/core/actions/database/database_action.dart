de400526908b2e666f4dc7f1fac590843260f900
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-10 18:34:55 +0200

    add database save and delete action

---
+++ lib/core/actions/database/database_action.dart
@@ -0,0 +1,7 @@
+import 'package:appsfactory_task/data/models/album_details_response.dart';
+
+class DatabaseAction {
+  AlbumDetailsResponse albumDetailsResponse;
+
+  DatabaseAction(this.albumDetailsResponse);
+}
