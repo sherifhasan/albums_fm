6e6ffb64d25070a1c9f36db3a08ffd000322eac7
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-10 18:44:11 +0200

    add dummy data for artist search testing

---
+++ test/data/models/artist_search/dummy_data.dart
@@ -0,0 +1,49 @@
+const artistSearchJsonString = """{
+  "results": {
+    "opensearch:Query": {
+      "#text": "",
+      "role": "request",
+      "searchTerms": "selena",
+      "startPage": "1"
+    },
+    "opensearch:totalResults": "13816",
+    "opensearch:startIndex": "0",
+    "opensearch:itemsPerPage": "1",
+    "artistmatches": {
+      "artist": [
+        {
+          "name": "Selena Gomez",
+          "listeners": "1467385",
+          "mbid": "e4bc69e2-a064-4f93-ada1-f7f209cc1cc3",
+          "url": "https://www.last.fm/music/Selena+Gomez",
+          "streamable": "0",
+          "image": [
+            {
+              "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+              "size": "small"
+            },
+            {
+              "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+              "size": "medium"
+            },
+            {
+              "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+              "size": "large"
+            },
+            {
+              "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+              "size": "extralarge"
+            },
+            {
+              "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+              "size": "mega"
+            }
+          ]
+        }
+      ]
+    },
+    "@attr": {
+      "for": "selena"
+    }
+  }
+} """;
