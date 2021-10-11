e16c9ffe2183ec4c6853d609255421a9b89251eb
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-11 14:01:30 +0200

    add details screen navigation in router

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,5 @@
+import 'package:appsfactory_task/core/screens/details/album_details_screen.dart';
+import 'package:appsfactory_task/data/models/album_details_response.dart';
 import 'package:flutter/material.dart';
 
 import 'core/screens/main/main_screen.dart';
@@ -13,6 +15,11 @@ class AppRouter {
           final artistName =
               ModalRoute.of(context)!.settings.arguments as String;
           return TopAlbumsScreen(artistName);
+        },
+        RoutePaths.details: (BuildContext context) {
+          final albumDetails = ModalRoute.of(context)!.settings.arguments
+              as AlbumDetailsResponse;
+          return AlbumDetailsScreen(albumDetails);
         }
       };
 }
