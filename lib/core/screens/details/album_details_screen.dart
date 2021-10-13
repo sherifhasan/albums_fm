7512615e79bf97874d7cb3fd1ffe0f6a5f084463
Sherifelsayed <sherif.elsayed@ureed.com>
2021-10-13 02:03:41 +0200

    update album details screen

--- lib/core/screens/details/album_details_screen.dart
+++ lib/core/screens/details/album_details_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' as ui;
-
 import 'package:appsfactory_task/core/actions/database/database_action.dart';
 import 'package:appsfactory_task/database_setup.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -25,127 +23,74 @@ class AlbumDetailsScreen extends StatelessWidget {
         builder: (BuildContext context, Store<AppState> store) {
       return SafeArea(
         child: Scaffold(
-          body: Stack(fit: StackFit.expand, children: [
-            Image.network(
-              album.imageList[3].text!,
-              fit: BoxFit.cover,
-            ),
-            BackdropFilter(
-              filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-              child: Container(
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-            Stack(
-              children: [
-                SingleChildScrollView(
-                  child: Container(
-                    margin: const EdgeInsets.all(16.0),
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          alignment: Alignment.center,
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
+          appBar: AppBar(
+            title: Text(album.name),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                  onPressed: () => store.dispatch(DatabaseAction(album)),
+                  icon: Icon(
+                    favouriteBox.containsKey('${album.artist}${album.name}')
+                        ? Icons.favorite
+                        : Icons.favorite_outline,
+                    color:
+                        favouriteBox.containsKey('${album.artist}${album.name}')
+                            ? Colors.red
+                            : Colors.white,
+                    size: 32,
+                  )),
+            ],
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(16),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: album.imageList != null
+                        ? CachedNetworkImage(
+                            imageUrl: album.imageList![3].text!,
+                            height: 150,
+                            fit: BoxFit.cover,
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
                               child: SizedBox(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: CachedNetworkImage(
-                                    imageUrl: album.imageList[3].text!,
-                                    height: 300,
-                                    width: 300,
-                                    fit: BoxFit.cover,
-                                    progressIndicatorBuilder:
-                                        (context, url, downloadProgress) =>
-                                            Center(
-                                      child: SizedBox(
-                                        height: 32,
-                                        width: 32,
-                                        child: CircularProgressIndicator(
-                                            value: downloadProgress.progress),
-                                      ),
-                                    ),
-                                    errorWidget: (context, url, error) =>
-                                        const Icon(Icons.error),
-                                  ))),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              boxShadow: const [
-                                BoxShadow(
-                                    color: Colors.black,
-                                    blurRadius: 20.0,
-                                    offset: Offset(0.0, 10.0))
-                              ]),
-                        ),
-                        Container(
-                          alignment: Alignment.center,
-                          margin: const EdgeInsets.symmetric(vertical: 20.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  album.name,
-                                  maxLines: 2,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 28.0,
-                                  ),
-                                ),
+                                height: 32,
+                                width: 32,
+                                child: CircularProgressIndicator(
+                                    value: downloadProgress.progress),
                               ),
-                              IconButton(
-                                  onPressed: () =>
-                                      store.dispatch(DatabaseAction(album)),
-                                  icon: Icon(
-                                    favouriteBox.containsKey(album.mbid)
-                                        ? Icons.favorite
-                                        : Icons.favorite_outline,
-                                    color: favouriteBox.containsKey(album.mbid)
-                                        ? Colors.red
-                                        : Colors.white,
-                                    size: 32,
-                                  )),
-                            ],
-                          ),
-                        ),
-                        const Text(
-                          'Tracks',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        const SizedBox(height: 8),
-                        if (album.tracks != null &&
-                            album.tracks!.tracksList != null)
-                          SizedBox(
-                              height: 250,
-                              child: TracksList(album.tracks!.tracksList!)),
-                        if (album.tracks == null)
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              'No tracks found',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w700),
                             ),
-                          ),
-                      ],
+                            errorWidget: (context, url, error) => Image.asset(
+                              "assets/image_not_found.png",
+                              height: 100,
+                            ),
+                          )
+                        : Image.asset(
+                            "assets/image_not_found.png",
+                            height: 100,
+                          )),
+              ),
+              const Text(
+                'Tracks',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 8),
+              album.tracks != null
+                  ? Expanded(flex:1,child: TracksList(album.tracks!))
+                  : const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'No tracks found',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700),
+                      ),
                     ),
-                  ),
-                ),
-                IconButton(
-                    onPressed: () => navigationService.pop(),
-                    icon: const Icon(
-                      CupertinoIcons.back,
-                      color: Colors.white,
-                      size: 32,
-                    )),
-              ],
-            )
-          ]),
+            ],
+          ),
         ),
       );
     });
